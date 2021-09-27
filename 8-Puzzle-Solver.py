6f35f89b62bdf270cef139e2a61ca326ba1ee134
matteoNunz <matteonunziante9999@gmail.com>
2021-09-27 15:21:08 +0200

    Added the possibility of different sizes

--- 8-Puzzle-Solver.py
+++ 8-Puzzle-Solver.py
@@ -18,16 +18,16 @@ Calculate the "distance" from the goal:
     g-score = number of nodes traversed from the start node to get to the
                 current node
 
-Input-> specified the start configuration as follow
+Input-> specified the size of the puzzle
+        specified the start configuration as follow
         1 2 3
         4 5 6
         7 8 _
-
-        The size of the matrix is 3, but it can be easily modified
 """
 
+
 class Node:
-    def __init__(self , data , level , fVal):
+    def __init__(self, data, level, fVal):
         """
         Initialize the node
         :param data: is the puzzle
@@ -43,23 +43,23 @@ class Node:
         Generate child nodes from the given node by moving the blank space
             either in the four directions {up,down,left,right}
         """
-        x , y = self.find(self.data , '_') # take the coordinate of the '_' character
+        x, y = self.find(self.data, '_')  # take the coordinate of the '_' character
         """
         valList contains position values for moving the blank space in either of
             the 4 directions [up,down,left,right] respectively
         """
-        valList = [[x , y - 1] , [x , y + 1] , [x - 1 , y] , [x + 1 , y]]
+        valList = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]]
         children = []
 
         # For every element in valList generate the child
         for val in valList:
-            child = self.shuffle(self.data , x , y , val[0] , val[1]) # Matrix , x , y , newX , newY
+            child = self.shuffle(self.data, x, y, val[0], val[1])  # Matrix , x , y , newX , newY
             if child is not None:
-                child_node = Node(child , self.level + 1 , 0)
+                child_node = Node(child, self.level + 1, 0)
                 children.append(child_node)
         return children
 
-    def shuffle(self , puz , x1 , y1 , x2 , y2):
+    def shuffle(self, puz, x1, y1, x2, y2):
         """
         Move the blank space in the given direction and if the position value are out
             of limits the return None
@@ -80,7 +80,7 @@ class Node:
         else:
             return None
 
-    def copy(self , root):
+    def copy(self, root):
         """
         Copy function to create a similar matrix of a given node
         :param root: the node to duplicate
@@ -95,20 +95,21 @@ class Node:
             duplicatePuz.append(duplicateRow)
         return duplicatePuz
 
-    def find(self , puz , x):
+    def find(self, puz, x):
         """
         Method used to find the coordinates of a specific character
         :param puz: is the puzzle (the matrix)
         :param x: is the char to find in the matrix
         :return: return the coordinates x and y of x in puz
         """
-        for i in range(0 , len(self.data)):
-            for j in range(0 , len(self.data)):
+        for i in range(0, len(self.data)):
+            for j in range(0, len(self.data)):
                 if puz[i][j] == x:
-                    return i , j
+                    return i, j
+
 
 class Puzzle:
-    def __init__(self , size):
+    def __init__(self, size):
         """
         Initialize the puzzle size by the specified size,open and closed lists to empty
         :param size: is the size of the puzzle
@@ -124,21 +125,21 @@ class Puzzle:
         :return: the puzzle
         """
         puz = []
-        for i in range(0 , self.size):
+        for i in range(0, self.size):
             temp = input().split(" ")
             puz.append(temp)
         return puz
 
-    def f(self , start , goal):
+    def f(self, start, goal):
         """
         Heuristic Function to calculate heuristic value f(x) = h(x) + g(x)
         :param start: h(x)
         :param goal: g(x)
         :return: return the f value
         """
-        return self.hProportional(start.data , goal) + start.level
+        return self.hProportional(start.data, goal) + start.level
 
-    def hPosition(self , start , goal):
+    def hPosition(self, start, goal):
         """
         Calculate the difference between the current puzzle and the final one -> check just if a number is in the right position
         :param start: the current node
@@ -146,13 +147,13 @@ class Puzzle:
         :return: return the h value
         """
         temp = 0
-        for i in range(0 , self.size):
-            for j in range(0 , self.size):
+        for i in range(0, self.size):
+            for j in range(0, self.size):
                 if start[i][j] != goal[i][j] and start[i][j] != '_':
                     temp += 1
         return temp
 
-    def hProportional(self , start , goal):
+    def hProportional(self, start, goal):
         """
         Calculate the difference between the current puzzle and the final one -> average position
         :param start:  the current node
@@ -162,12 +163,12 @@ class Puzzle:
         value = 0
         # Create the node corresponding to the final configuration
         goalNode = Node(goal, 0, 0)
-        for i in range(0 , self.size):
-            for j in range(0 , self.size):
-                # Take the coordinate if the number start[i][j] in the goal configuation
-                x , y = goalNode.find(goal , start[i][j])
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                # Take the coordinate if the number start[i][j] in the goal configuration
+                x, y = goalNode.find(goal, start[i][j])
                 # Calculate the sqrt of the distance between the numbers
-                value += ((abs(x - i)**2 + abs(y - j)**2))**(1/2)
+                value += ((abs(x - i) ** 2 + abs(y - j) ** 2)) ** (1 / 2)
         return value
 
     def process(self):
@@ -178,11 +179,22 @@ class Puzzle:
         start = self.accept()
         # print("Enter the goal state matrix \n")
         # goal = self.accept()
-        goal = [['1' , '2' , '3'] , ['4' , '5' , '6'] , ['7' , '8' , '_']]
+        # goal = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '_']]
+
+        # Generate the goal matrix
+        goal = []
+        for i in range(0 , self.size):
+            goalRow = []
+            for j in range(0 , self.size):
+                if i == self.size - 1 and j == self.size - 1:
+                    goalRow.append('_')
+                else:
+                    goalRow.append(str(i * self.size + j + 1))
+            goal.append(goalRow)
 
         # Create the node
-        start = Node(start , 0 , 0)
-        start.fVal = self.f(start , goal)
+        start = Node(start, 0, 0)
+        start.fVal = self.f(start, goal)
 
         # Put the start node in the open list
         self.openList.append(start)
@@ -199,15 +211,15 @@ class Puzzle:
             print(" \\\'/ \n")
             for row in cur.data:
                 for i in row:
-                    print(i, end = " ")
+                    print(i, end=" ")
                 print("")
 
             """
             If the difference between current and goal node is 0 we have reached the goal node
             Position method for h is more efficient in that case
             """
-            if self.hPosition(cur.data , goal) == 0:
-               break
+            if self.hPosition(cur.data, goal) == 0:
+                break
 
             for node in cur.generateChild():
                 node.fVal = self.f(node, goal)
@@ -218,9 +230,9 @@ class Puzzle:
             """
             Sort the open list based on the f value
             """
-            self.openList.sort(key = lambda x : x.fVal , reverse = False)
+            self.openList.sort(key=lambda x: x.fVal, reverse=False)
 
-    def addNode(self , node):
+    def addNode(self, node):
         """
         Add a node if it isn't already in the openList and in the closedList
         :param node: is the new node to add
@@ -242,7 +254,17 @@ class Puzzle:
 
 # Start the game
 if __name__ == '__main__':
-    puz = Puzzle(3)
+
+    size = 0
+    while True:
+        print("Insert the size of the matrix")
+        try:
+            size = int(input())
+            break
+        except:
+            print("Bad format")
+    puz = Puzzle(size)
+
     puz.process()
 
     print("Element in the closed list are: " + str(len(puz.closedList)))
